82546d0528dda7a983cd91b2a33cfcd838f55edf
pmihango <pmihango@gmail.com>
2023-02-21 11:33:23 +0300

    added the Author class and methods

---
+++ lib/author.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+
+
+class Author
+attr_reader :name, :articles
+
+@@all = []
+
+    def initialize(name)
+        @name = name
+        @articles = []
+        @@all << self
+    end
+
+    def self.all
+        @@all
+      end
+    
+     def add_article(magazine, title)
+        new_article = Article.new(self, magazine, title) 
+        @articles << new_article
+        magazine.add_contributor(self)
+    end
+
+    def magazines
+    @articles.map { |article| article.magazine}.uniq
+    
+    end
+end
